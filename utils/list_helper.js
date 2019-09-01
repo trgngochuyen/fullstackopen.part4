17228908225da0f5a299110e747b16f5f68c74ee
Truong Ngoc Huyen <trg.ngochuyen@gmail.com>
2019-09-01 21:35:46 +0300

    Submit exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    const likesList = blogs.map(blog => blog.likes)
+    return likesList.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
