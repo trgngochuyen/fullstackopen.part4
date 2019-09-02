a5e70aac3b54bbd80206be6f75dd8537b8f22d8e
Truong Ngoc Huyen <trg.ngochuyen@gmail.com>
2019-09-02 11:42:20 +0300

    Submit exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,25 @@ const totalLikes = (blogs) => {
     return likesList.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    let favBlog = blogs[0]
+    blogs.forEach(blog => {
+        if(blog.likes > favBlog.likes) {
+            favBlog = blog
+        }
+    })
+    return {
+        title: favBlog.title,
+        author: favBlog.author,
+        likes: favBlog.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
