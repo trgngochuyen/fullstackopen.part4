683163161aa0ca93359e7ee9448a817546298754
Truong Ngoc Huyen <trg.ngochuyen@gmail.com>
2019-09-05 09:44:07 +0300

    Submit exercise 4.11

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -34,47 +34,50 @@ test('unique identifier property is named id', async () => {
     expect(identifier).toBeDefined()
 })
 
-test.only('a valid blog can be added', async () => {
-    const newBlog = {
-        title: 'Porkkanakakku',
-        author: 'Yan',
-        url: 'http://localhost:3003/api/blogs/9',
-        likes: 829
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+describe.only('test HTTP POST request', async => {
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            title: 'Porkkanakakku',
+            author: 'Yan',
+            url: 'http://localhost:3003/api/blogs/9',
+            likes: 829
+        }
     
-    const afterAdding = await helper.blogsInDb()
-    const titles = afterAdding.map(r => r.title)
-    const authors = afterAdding.map(r => r.author)
-    const numberOfBlogsAfter = afterAdding.length
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        const afterAdding = await helper.blogsInDb()
+        const titles = afterAdding.map(r => r.title)
+        const authors = afterAdding.map(r => r.author)
+        const numberOfBlogsAfter = afterAdding.length
+        
+        // if the new blog is successfully added, the number of documents in the collection increases by 1,
+        // and its title and author will be included
 
-    expect(numberOfBlogsAfter).toBe(helper.initialBlogs.length +1)
-    expect(titles).toContain('Porkkanakakku')
-    expect(authors).toContain('Yan')
-})
+        expect(numberOfBlogsAfter).toBe(helper.initialBlogs.length +1)
+        expect(titles).toContain('Porkkanakakku')
+        expect(authors).toContain('Yan')
+    })
+    
+    test('missing likes will have default value of zero', async () => {
+        const newBlog = {
+            title: 'Porkkanakakku ja hillo',
+            author: 'Jenni',
+            url: 'http://localhost:3003/api/blogs/9',
+        }
+    
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+        
+        // if likes property is missing from the request, default value will be 0
 
-test('missing likes will have default value of zero', async () => {
-    const newBlog = {
-        title: 'Porkkanakakku ja hillo',
-        author: 'Jenni',
-        url: 'http://localhost:3003/api/blogs/9',
-        likes: undefined
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-
-    const afterAdding = await helper.blogsInDb()
-    const likes = afterAdding.filter(blog => blog.likes===0)
-    expect(afterAdding.length).toBe(helper.initialBlogs.length+1)
-    expect(likes).toBeDefined()
+        expect(response.body.likes).toBe(0)
+    })
 })
 
 afterAll(() => {
